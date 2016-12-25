cdba485800032ff6bbfa12f96caf01222056b5d9
Philip Winchester <philipwinc@gmail.com>
2016-12-25 09:39:14 -0500

    added landing graphic

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 (() => {
-  console.log('script loaded');
+  let landNav = document.querySelector('div.land-nav');
+  let talentDiv = document.querySelector('.talents');
+  landNav.addEventListener('click', () => {
+    console.log('giving a cuppa');
+    // let body = document.getElementsByTagName("BODY")[0];
+    // body.removeChild(landNav)
+    landNav.className = 'hidden';
+    talentDiv.className = 'talents';
+  })
 })();
